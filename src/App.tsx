ab26e9ffbafa4fcaecec9211798f75b59cafef2e
HienLe2004 <hien.lengoc2004@hcmut.edu.vn>
2025-03-08 13:31:40 +0700

    Setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <h1 className="text-3xl font-bold underline text-red-600">
+          Simple React Typescript Tailwind Sample
+        </h1>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
